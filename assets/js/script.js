b8d72e47824b5c9a55d69dc65b8da950543870b2
perun-01 <singhranjeet5200@gmail.com>
2019-12-28 15:24:49 +0530

    Enhanced Website design

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+// JavaScript
+var motto = "Eat. Code. Sleep. Repeat.";
+var mottoLength = 0;
+var mottoPrint;
+
+$(document).ready(function() {
+  setInterval("cursorAnimation()", 600);
+  mottoPrint = $("#motto");
+  setInterval("typeNerase()", 175);
+  $('a[href*="#"]:not([href="#"])').click(function() {
+   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     if (target.length) {
+       $('html, body').animate({
+         scrollTop: target.offset().top
+       }, 1000);
+       return false;
+     }
+   }
+ });
+
+function cursorAnimation() {
+  $("#cursor").animate({
+    opacity: 0
+  }, "fast", "swing").animate({
+    opacity: 1
+  }, "fast", "swing");
+}
+
+function erase() {
+  mottoPrint.html(motto.substr(0, mottoLength--));
+  if (mottoLength >= 0) {
+    setTimeout("erase()", 20);
+  } else {
+    mottoLength = 0;
+  }
+}
+
+function typeNerase() {
+  mottoPrint.html(motto.substr(0, mottoLength++));
+  if (mottoLength >= motto.length + 1) {
+    erase();
+  }
+}
